cd5e35367c5121c67ede9e4e1206a01c01f32f1c
remon <remmony@gmail.com>
2023-01-30 15:36:31 +0100

    create release

--- bosh/ruby-app-release/unit-tests/spec/helpers.rb
+++ bosh/ruby-app-release/unit-tests/spec/helpers.rb
@@ -1,20 +1,18 @@
 OPEN_TOKEN = /.+\)/
-CLOSE_TOKEN = /^\s*\;\;\s*$/
+CLOSE_TOKEN = /^\s*;;\s*$/
 CASE_TOKEN = /\s*case.*/
 
 def check_block_formations(rendered_template)
-
   line_index = 0
-  tokens = Array.new #acts like a stack
-  
-  #we only keep three types of tokens in the stack
+  tokens = [] # acts like a stack
+
+  # we only keep three types of tokens in the stack
   rendered_template.each_line do |line|
-    case
-    when line.match?(CASE_TOKEN)
+    if line.match?(CASE_TOKEN)
       tokens.push(CASE_TOKEN)
-    when line.match?(OPEN_TOKEN)
+    elsif line.match?(OPEN_TOKEN)
       tokens.push(OPEN_TOKEN)
-    when line.match?(CLOSE_TOKEN)
+    elsif line.match?(CLOSE_TOKEN)
       tokens.push(CLOSE_TOKEN)
     end
   end
@@ -23,16 +21,17 @@ def check_block_formations(rendered_template)
   # if not, then the rest of the tokens are not within a CASE..ESAC block, which is not allowed
   case_token = tokens.shift
   return false unless case_token == CASE_TOKEN
-  
+
   remaining_tokens_count = tokens.length
   next_token = CLOSE_TOKEN
   # pop in LIFO order and see whether tokens are matched
-  # i.e if the current token is OPEN, the next one must be CLOSE. 
+  # i.e if the current token is OPEN, the next one must be CLOSE.
   for i in 1..remaining_tokens_count do
 
     t = tokens.pop
-    return false unless t == next_token
-    next_token == CLOSE_TOKEN ? next_token = OPEN_TOKEN : next_token = CLOSE_TOKEN
+    return false unless t != next_token
+
+    next_token = next_token == CLOSE_TOKEN ? OPEN_TOKEN : CLOSE_TOKEN
   end
   true
 end
@@ -41,12 +40,9 @@ def find_statement(rendered_template, statement)
   statement_line = -1
   index = 0
   rendered_template.each_line do |line|
-    if line.strip.match? statement
-      statement_line = index
-    end
+    statement_line = index if line.strip.match? statement
     index += 1
   end
-  
-  statement_line  
-end
 
+  statement_line
+end
--- bosh/ruby-app-release/unit-tests/spec/job_spec.rb
+++ bosh/ruby-app-release/unit-tests/spec/job_spec.rb
@@ -5,17 +5,15 @@ require 'yaml'
 require_relative 'helpers'
 
 describe 'ruby app main job:' do
-
   let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
   let(:job) { release.job('rubyweb') }
 
   describe 'control template' do
-
     it 'raises error if control script is not put in the right directory inside the VM instance (check the job spec file for typos or misnaming!)' do
       right_dir = true
-      begin 
+      begin
         job.template('bin/ctl')
-      rescue
+      rescue StandardError
         right_dir = false
       end
 
@@ -25,78 +23,76 @@ describe 'ruby app main job:' do
     let(:template) { job.template('bin/ctl') }
 
     it 'raises error if empty bootstrap is provided' do
-      expect {template.render("bootstrap" => "")}.to raise_error 'No bootstrap file provided'
+      expect { template.render('bootstrap' => '') }.to raise_error 'No bootstrap file provided'
     end
 
     it 'raises error if bootstrap file is not the correct one' do
-      expect {template.render("bootstrap" => "config.ru")}.to raise_error 'Wrong bootstrap file provided'
+      expect { template.render('bootstrap' => 'config.ru') }.to raise_error 'Wrong bootstrap file provided'
     end
 
-    it 'raises error if control script is malformed' do 
-      tmps = template.render("bootstrap" => "app.rb")
-      expect(tmps.lines[0]).to include ("#!/bin/bash")
+    it 'raises error if control script is malformed' do
+      tmps = template.render('bootstrap' => 'app.rb')
+      expect(tmps.lines[0]).to include('#!/bin/bash')
     end
-    
+
     it 'raises error if exec command is malformed' do
-      tmps = template.render("bootstrap" => "app.rb")
+      tmps = template.render('bootstrap' => 'app.rb')
 
       exec_line = tmps.each_line do |line|
-        break if line.include? "bundle exec"
+        break if line.include? 'bundle exec'
       end
-      
-      expect(exec_line).to include("bundle exec ruby app.rb")
+
+      expect(exec_line).to include('bundle exec ruby app.rb')
     end
 
-    it 'raises error if stop block is not defined' do 
-      tmps = template.render("bootstrap" => "app.rb")
+    it 'raises error if stop block is not defined' do
+      tmps = template.render('bootstrap' => 'app.rb')
       expect(find_statement(tmps, /^stop\)$/)).not_to eq(-1)
     end
 
     it 'raises error if main process is not killed inside the stop block' do
-      tmps = template.render("bootstrap" => "app.rb")
+      tmps = template.render('bootstrap' => 'app.rb')
 
       stop_block_line = find_statement(tmps, /^stop\)$/)
-      kill_statement_line = find_statement(tmps, /kill\s-[\d]\s`cat\s\$PIDFILE`/)
+      kill_statement_line = find_statement(tmps, /kill\s-\d\s`cat\s\$PIDFILE`/)
       expect(stop_block_line).to be < kill_statement_line
     end
 
     it 'raises error if case blocks are not formed correctly' do
-      tmps = template.render("bootstrap" => "app.rb")
-      expect(check_block_formations(tmps)).to be true 
+      tmps = template.render('bootstrap' => 'app.rb')
+      expect(check_block_formations(tmps)).to be true
     end
-
   end
 
-  describe 'config template' do 
-
+  describe 'config template' do
     it 'raises error if config template is not put in the right directory inside the VM instance' do
       right_dir = true
-      begin 
+      begin
         job.template('cfg/config.yml')
-      rescue
+      rescue StandardError
         right_dir = false
       end
 
       expect(right_dir).to be true
     end
 
-    let(:conf_template) {job.template('cfg/config.yml')}
+    let(:conf_template) { job.template('cfg/config.yml') }
 
     it 'raises error if configs are wrong' do
-      expect {conf_template.render("port" => 1024)}.to raise_error "Invalid port number"
-      expect {conf_template.render("port" => 7999)}.to raise_error "Invalid port number"
-      expect {conf_template.render("port" => 8080)}.not_to raise_error 
+      expect { conf_template.render('port' => 1024) }.to raise_error 'Invalid port number'
+      expect { conf_template.render('port' => 7999) }.to raise_error 'Invalid port number'
+      expect { conf_template.render('port' => 8080) }.not_to raise_error
     end
 
-    it 'raises error if yml is not parsable or malformed' do 
+    it 'raises error if yml is not parsable or malformed' do
       parsable = true
       begin
-        yml = YAML.load(conf_template.render("port" => 8080))
+        yml = YAML.load(conf_template.render('port' => 8080))
       rescue StandardError => e
         parsable = false
       end
-        expect(parsable).to be true
-        expect(yml["port"]).to eq(8080)
+      expect(parsable).to be true
+      expect(yml['port']).to eq(8080)
     end
   end
 end
